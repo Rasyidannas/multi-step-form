3e19d4b6f72201c29d5b8268aa88739ac05acf68
rasyid annas <rasshit.dsgn@gmail.com>
2023-10-23 17:41:51 +0700

    feat: confirm button have done

--- src/App.js
+++ src/App.js
@@ -13,9 +13,12 @@ import Button from "./components/UI/Button";
 
 export default function App() {
   //validation for each form component
-  const[infoValid, setInfoValid] = useState(false)
-  const[planValid, setPlanValid] = useState(false)
-  const[addValid, setAddValid] = useState(false)
+  const [infoValid, setInfoValid] = useState(false);
+  const [planValid, setPlanValid] = useState(false);
+  const [addValid, setAddValid] = useState(false);
+
+  //confimation
+  const [confirm, setConfirm] = useState(false);
 
   const [formActive, setFormActive] = useState(0);
 
@@ -31,57 +34,75 @@ export default function App() {
   //handler for submit
   const submitHandler = (event) => {
     event.preventDefault();
-  }
+
+    setConfirm(true)
+  };
 
   return (
     <FormProvider>
       <Card className="flex w-4/5 h-screen mx-auto app rounded-3xl">
         <SideNav className="flex-none" currForm={formActive} />
-        <form className="flex flex-col items-end justify-between flex-1 w-8/12 mx-12 mt-8 mb-4 overflow-hidden" onSubmit={submitHandler}>
-          <div
-            className="flex w-full transition-all duration-300"
-            style={{ transform: `translateX(-${formActive * 100}%)` }}
+        {!confirm && (
+          <form
+            className="flex flex-col items-end justify-between flex-1 w-8/12 mx-12 mt-8 mb-4 overflow-hidden"
+            onSubmit={submitHandler}
           >
-            <PersonalInfo className="flex-none p-2" formIsValid={(valid) => setInfoValid(valid)} />
-            <SelectPlan className="flex-none p-2" formIsValid={(valid) => setPlanValid(valid)} />
-            <PickAddOns className="flex-none p-2" formIsValid={(valid) => setAddValid(valid)}/>
-            <FinishingUp
-              className="flex-none p-2"
-              onChange={() => setFormActive(1)}
-            />
-          </div>
-          {/* <SuccessForm /> */}
-          <div className="flex justify-between w-full">
-            {formActive !== 0 && (
-              <Button
-                type="button"
-                className="text-cool-gray"
-                onClick={prevHandler}
-              >
-                Go Back
-              </Button>
-            )}
-            {formActive !== 3 && (
-              <Button
-                type="button"
-                className="ml-auto text-white bg-marine-blue"
-                onClick={nextHandler}
-                disabled={
-                    formActive === 0 && !infoValid ||
-                    formActive === 1 && !planValid ||
-                    formActive === 2 && !addValid
+            <div
+              className="flex w-full transition-all duration-300"
+              style={{ transform: `translateX(-${formActive * 100}%)` }}
+            >
+              <PersonalInfo
+                className="flex-none p-2"
+                formIsValid={(valid) => setInfoValid(valid)}
+              />
+              <SelectPlan
+                className="flex-none p-2"
+                formIsValid={(valid) => setPlanValid(valid)}
+              />
+              <PickAddOns
+                className="flex-none p-2"
+                formIsValid={(valid) => setAddValid(valid)}
+              />
+              <FinishingUp
+                className="flex-none p-2"
+                onChange={() => setFormActive(1)}
+              />
+            </div>
+            {/* <SuccessForm /> */}
+            <div className="flex justify-between w-full">
+              {formActive !== 0 && (
+                <Button
+                  type="button"
+                  className="text-cool-gray"
+                  onClick={prevHandler}
+                >
+                  Go Back
+                </Button>
+              )}
+              {formActive !== 3 && (
+                <Button
+                  type="button"
+                  className="ml-auto text-white bg-marine-blue"
+                  onClick={nextHandler}
+                  disabled={
+                    (formActive === 0 && !infoValid) ||
+                    (formActive === 1 && !planValid) ||
+                    (formActive === 2 && !addValid)
                   }
-              >
-                Next Step
-              </Button>
-            )}
-            {formActive === 3 && (
-              <Button type="submit" className="text-white bg-marine-blue">
-                Confirm
-              </Button>
-            )}
-          </div>
-        </form>
+                >
+                  Next Step
+                </Button>
+              )}
+              {formActive === 3 && (
+                <Button type="submit" className="text-white bg-marine-blue">
+                  Confirm
+                </Button>
+              )}
+            </div>
+          </form>
+        )}
+
+        {confirm && <SuccessForm />}
       </Card>
     </FormProvider>
   );
